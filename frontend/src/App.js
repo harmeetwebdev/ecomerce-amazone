63fbbeb5e44c8ffd8c17ab6c5cbc061cfe5a5b51
Harmeet Bansal <harmeets627@gmail.com>
2022-11-11 11:20:27 +0530

    video-04-data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import data from './data';
 import './App.css';
 
 function App() {
@@ -8,9 +8,25 @@ function App() {
         <a href='/'>amazone</a>
       </header>
       <main>
-        list products
-      </main>
-    </div>
+        <h1>Featured product</h1>
+        <div className='products'>
+          {data.products.map(product => (
+            < div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main >
+    </div >
   );
 }
 
